08d61761cbe4bf2eb3651ba9be79e1dc4b837ee0
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-03-13 12:58:02 +0000

    SPA part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({ dish }) {
@@ -43,6 +44,16 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
             return(
                 <div className="container">
                     <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                    <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             {/* dish */}
                             <RenderDish dish={props.dish} />
@@ -50,7 +61,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                         <div className="col-12 col-md-5 m-1">
                             {/* comments */}
                             <h4>Comments</h4>
-                            <RenderComments comments={props.dish.comments} />
+                            <RenderComments comments={props.comments} />
                         </div>
                     </div>
                 </div>             
