f36732307487b6f1af4b820c56fd65d864004f34
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-03-14 13:11:07 +0000

    into to redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { createRef } from 'react';
+import { createStore } from 'redux';
+import { Reducer, initialState } from './reducer';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        Reducer, 
+        initialState
+    );
+
+    return store;
+}
